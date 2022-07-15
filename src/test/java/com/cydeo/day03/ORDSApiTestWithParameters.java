734848fab29ae54ea98750145f75797fb30a9f88
hrnoksz <harun1973@yahoo.com>
2022-07-15 13:45:18 +0300

    Day 03: ORDSApi test1 is done

---
+++ src/test/java/com/cydeo/day03/ORDSApiTestWithParameters.java
@@ -0,0 +1,45 @@
+package com.cydeo.day03;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ORDSApiTestWithParameters {
+
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we don't need to type each http method.
+        baseURI = "http://3.83.123.243:1000/ords/hr";
+    }
+    /*
+        Given accept type is Json
+        And parameters: q = {"region_id":2}
+        When users sends a GET request to "/countries"
+        Then status code is 200
+        And Content type is application/json
+        And Payload should contain "United States of America"
+     */
+
+    @DisplayName("GET request to /countries with Query Param")
+    @Test
+    public void test1(){
+
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParam("q", "{\"region_id\":2}")
+                .when().get("/countries");
+
+        //verify status code
+        assertEquals(200, response.statusCode());
+
+        //verify content type
+        assertEquals("application/json", response.contentType());
+
+        //verify "United States of America" in the json payload/body
+        assertTrue(response.body().asString().contains("United States of America"));
+    }
+}
