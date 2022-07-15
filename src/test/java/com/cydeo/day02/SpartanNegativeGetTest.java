4fd9417de4a002cc2160168b70896f59a5112807
hrnoksz <harun1973@yahoo.com>
2022-07-15 09:51:45 +0300

    Day 03 test one is done

--- src/test/java/com/cydeo/day02/SpartanNegativeGetTest.java
+++ src/test/java/com/cydeo/day02/SpartanNegativeGetTest.java
@@ -1,12 +1,12 @@
 package com.cydeo.day02;
 
-import io.restassured.RestAssured;
+
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
