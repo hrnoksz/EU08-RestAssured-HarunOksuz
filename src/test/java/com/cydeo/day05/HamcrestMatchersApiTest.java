9cb9f98ca0fcd2af605f8160344938d69e67aa64
hrnoksz <harun1973@yahoo.com>
2022-07-19 10:43:59 +0300

    Day 05: HamcrestMatchersApiTest.java second case is done

--- src/test/java/com/cydeo/day05/HamcrestMatchersApiTest.java
+++ src/test/java/com/cydeo/day05/HamcrestMatchersApiTest.java
@@ -29,18 +29,40 @@ public class HamcrestMatchersApiTest {
     public void test1() {
 
                 given().
-                        accept(ContentType.JSON)
-                        .and().pathParam("id", 15)
+                    accept(ContentType.JSON)
+                    .and().pathParam("id", 15)
                 .when()
-                        .get("http://3.83.123.243:8000/api/spartans/{id}")
+                    .get("http://3.83.123.243:8000/api/spartans/{id}")
                 .then()
-                        .statusCode(200)
-                        .and().assertThat()
-                        .contentType("application/json")
-                        .and()
-                        .body("id", is(15),
+                    .statusCode(200)
+                    .and().assertThat()
+                    .contentType("application/json")
+                    .and()
+                    .body("id", is(15),
                         "name", is("Meta"), "gender", is("Female"),
                         "phone", is(1938695106));
 
+    }
+
+    @DisplayName("CBTraining Teacher request with chaining and matchers")
+    @Test
+    public void test2() {
+
+                given()
+                    .accept(ContentType.JSON)
+                    .and().pathParam("id", 21887)
+                .when()
+                    .get("http://api.cybertektraining.com/teacher/{id}")
+                .then()
+                    .statusCode(200).and()
+                    .contentType("application/json")
+                    .and()
+                    //.header("Content-Length", is("275")) There is a problem related to website
+                    .and()
+                    .header("Date", notNullValue())
+                    .and().assertThat()
+                    .body("teachers[0].firstName", is("Leonel"))
+                    .body("teachers[0].lastName", is("Messi"))
+                    .body("teachers[0].gender", is("Male"));
     }
 }
