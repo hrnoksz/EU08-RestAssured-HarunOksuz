47a74af92d7505774d7108694ca895aeef34bb4f
hrnoksz <harun1973@yahoo.com>
2022-07-20 13:33:40 +0300

    Day6: Lombok dependency added

--- src/test/java/com/cydeo/day06/pojo/Link.java
+++ src/test/java/com/cydeo/day06/pojo/Link.java
@@ -1,30 +1,17 @@
 package com.cydeo.day06.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
 public class Link {
 
     private String rel;
     private String href;
 
-    public String getRel() {
-        return rel;
-    }
 
-    public void setRel(String rel) {
-        this.rel = rel;
-    }
-
-    public String getHref() {
-        return href;
-    }
-
-    public void setHref(String href) {
-        this.href = href;
-    }
-
-    public String toString() {
-        return "link{" +
-                "rel='" + rel + '\'' +
-                ", href='" + href + '\'' +
-                '}';
-    }
 }
