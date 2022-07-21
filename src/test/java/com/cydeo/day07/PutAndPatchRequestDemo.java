db3c75e13c5edb81ed8492d519e1c885b67136fe
hrnoksz <harun1973@yahoo.com>
2022-07-21 13:47:04 +0300

    Day7: PutAndPatchRequestDemo.java patchRequest is done

--- src/test/java/com/cydeo/day07/PutAndPatchRequestDemo.java
+++ src/test/java/com/cydeo/day07/PutAndPatchRequestDemo.java
@@ -26,6 +26,12 @@ public class PutAndPatchRequestDemo extends SpartanTestBase {
         putRequestMap.put("gender", "Male");
         putRequestMap.put("phone", 8811111111L);
 
+        /*
+        When we use PUT request, we should provide all data's inside JSON body.
+        It doesn't matter whether we update or not them.
+        In above example, we update only phone number, but we provide all data's
+         */
+
        given().contentType(ContentType.JSON)
                 .body(putRequestMap).log().all()
                 .and().pathParam("id", 117)
@@ -45,6 +51,26 @@ public class PutAndPatchRequestDemo extends SpartanTestBase {
         assertThat(putRequestMap.get("phone"), is(spartanPosted.getPhone()));
 
     }
+    @DisplayName("PATCH request to one spartan for partial update with Map")
+    @Test
+    public void patchRequest() {
+        //just like post request we have different options to send body, we will go with map
+        Map<String, Object> putRequestMap = new LinkedHashMap<>();
+        putRequestMap.put("phone", 8811111111L);
 
+        /*
+        When we use PATCH request, we don't need to provide all data's.
+        We provide only data's that we only update them.
+        It is possible to update more than one data
+         */
+
+        given().contentType(ContentType.JSON)
+                .body(putRequestMap).log().all()
+                .and().pathParam("id", 117)
+                .when().patch("/api/spartans/{id}")
+                .then()
+                .statusCode(204);
+
+    }
 
 }
