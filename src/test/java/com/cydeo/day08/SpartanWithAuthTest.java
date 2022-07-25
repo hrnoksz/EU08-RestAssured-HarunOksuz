d8f489404da77ba34e0dcc546442d0ab227a754a
hrnoksz <harun1973@yahoo.com>
2022-07-25 10:36:35 +0300

    Day08 new SpartanAuthTestBase.java and SpartanWithAuthTest.java are added

---
+++ src/test/java/com/cydeo/day08/SpartanWithAuthTest.java
@@ -0,0 +1,4 @@
+package com.cydeo.day08;
+
+public class SpartanWithAuthTest {
+}
---
+++ src/test/java/com/cydeo/utililities/SpartanAuthTestBase.java
@@ -0,0 +1,23 @@
+package com.cydeo.utililities;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
+public class SpartanAuthTestBase {
+
+    @BeforeAll
+    public static void init(){ //MUST BE STATIC!!!!!!!!!!!!!!!!
+        //save baseurl inside this variable so that we don't need to type each http method.
+        baseURI = "http://3.83.123.243:7000";
+
+
+    }
+
+    @AfterAll //MUST BE STATIC!!!!!!!!!!
+    public static void teardown(){
+
+        //DBUtils.destroy();
+    }
+}
