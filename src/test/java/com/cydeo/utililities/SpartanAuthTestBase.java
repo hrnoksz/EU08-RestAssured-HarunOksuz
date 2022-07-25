8a771ddba32e27c3a1e353e8a4e395ae14556e07
hrnoksz <harun1973@yahoo.com>
2022-07-25 10:37:10 +0300

    Day08 new SpartanAuthTestBase.java and SpartanWithAuthTest.java are added

--- src/test/java/com/cydeo/utililities/SpartanAuthTestBase.java
+++ src/test/java/com/cydeo/utililities/SpartanAuthTestBase.java
@@ -8,16 +8,10 @@ import static io.restassured.RestAssured.baseURI;
 public class SpartanAuthTestBase {
 
     @BeforeAll
-    public static void init(){ //MUST BE STATIC!!!!!!!!!!!!!!!!
+    public static void init() { //MUST BE STATIC!!!!!!!!!!!!!!!!
         //save baseurl inside this variable so that we don't need to type each http method.
         baseURI = "http://3.83.123.243:7000";
 
-
     }
 
-    @AfterAll //MUST BE STATIC!!!!!!!!!!
-    public static void teardown(){
-
-        //DBUtils.destroy();
-    }
 }
