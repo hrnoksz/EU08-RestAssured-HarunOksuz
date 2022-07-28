89116a5f7e9cf047fffcd0b343c96be199f4316a
hrnoksz <harun1973@yahoo.com>
2022-07-28 12:00:06 +0300

    Day10 FormulaOneXmlTest.java added

--- src/test/java/com/cydeo/day10/FormulaOneXmlTest.java
+++ src/test/java/com/cydeo/day10/FormulaOneXmlTest.java
@@ -1,7 +1,9 @@
 package com.cydeo.day10;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -11,8 +13,18 @@ public class FormulaOneXmlTest {
 
     @BeforeAll
     public static void setUp(){
+        //http://ergast.com/api/f1/drivers/alonso
         baseURI ="http://ergast.com";
+        basePath = "/api/f1";
+    }
 
-
+    @Test
+    public void test1(){
+        Response response = given()
+                                .pathParam("driver", "alonso")
+                            .when()
+                                .get("/drivers/{driver}")
+                            .then().statusCode(200).log().all()
+                                    .extract().response();
     }
 }
