0971869c0eb8c5cd3c0812aa5cf15a4fbe8aef35
hrnoksz <harun1973@yahoo.com>
2022-07-28 12:20:10 +0300

    Day10 FormulaOneXmlTest.java test1 is done

--- src/test/java/com/cydeo/day10/FormulaOneXmlTest.java
+++ src/test/java/com/cydeo/day10/FormulaOneXmlTest.java
@@ -1,6 +1,7 @@
 package com.cydeo.day10;
 
 import io.restassured.RestAssured;
+import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -26,5 +27,29 @@ public class FormulaOneXmlTest {
                                 .get("/drivers/{driver}")
                             .then().statusCode(200).log().all()
                                     .extract().response();
+
+        XmlPath xmlPath = response.xmlPath();
+        //get given name
+        String givenName = xmlPath.getString("MRData.DriverTable.Driver.GivenName");
+        System.out.println("givenName = " + givenName);
+
+        //get family name
+        String familyName = xmlPath.getString("MRData.DriverTable.Driver.FamilyName");
+        System.out.println("familyName = " + familyName);
+
+        //if you are trying to get attribute we use @ sign!!!!!!!!!!!!!!!!
+        //get driverId
+        String driverId = xmlPath.getString("MRData.DriverTable.Driver.@driverId");
+        System.out.println("driverId = " + driverId);
+
+        //get code
+        String code = xmlPath.getString("MRData.DriverTable.Driver.@code");
+        System.out.println("code = " + code);
+        
+        //get url
+        String url = xmlPath.getString("MRData.DriverTable.Driver.@url");
+        System.out.println("url = " + url);
+
+
     }
 }
