677a3bfc9276b8482fe10ea0a7fc610f789d39e3
hrnoksz <harun1973@yahoo.com>
2022-07-29 11:05:45 +0300

    Day11 TestLifeCycleAnnotations.java added

--- src/test/java/com/cydeo/day10/ResponseTimeTest.java
+++ src/test/java/com/cydeo/day10/ResponseTimeTest.java
@@ -1,6 +1,8 @@
 package com.cydeo.day10;
 
 import com.cydeo.utililities.SpartanAuthTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,7 +14,16 @@ public class ResponseTimeTest extends SpartanAuthTestBase {
     @Test
     public void test1(){
 
+        Response response = given()
+                .auth().basic("admin", "admin")
+                .accept(ContentType.JSON)
+                .when()
+                .get("/api/spartans")
+                .then()
+                .time(both(greaterThan(500L)).and(lessThanOrEqualTo(1100L))) //!!!!!!!!!!!!!!!!!
+                .extract().response();
 
+        System.out.println("response.getTime() = " + response.getTime());
 
     }
 
---
+++ src/test/java/com/cydeo/day11/TestLifeCycleAnnotations.java
@@ -0,0 +1,9 @@
+package com.cydeo.day11;
+
+public class TestLifeCycleAnnotations {
+
+
+
+
+
+}
