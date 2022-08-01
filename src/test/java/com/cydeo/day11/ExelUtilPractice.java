06ffe01117e6b3e675617ae841ef1219642a2e44
hrnoksz <harun1973@yahoo.com>
2022-08-01 10:03:15 +0300

    Day11 Vytracktestdata.xlsx added

---
+++ src/test/java/com/cydeo/day11/ExelUtilPractice.java
@@ -0,0 +1,17 @@
+package com.cydeo.day11;
+
+import com.cydeo.utililities.ExcelUtil;
+import org.junit.jupiter.api.Test;
+
+public class ExelUtilPractice {
+
+    @Test
+    public void test1(){
+
+        //How to use excelUtil file ?
+        //it accepts two arguments
+        //Argument 1: location of the file(path)
+        //Argument 2: sheet that we want to open
+        ExcelUtil vytrackFile = new ExcelUtil("src/test/resources/Vytracktestdata.xlsx", "QA3-short");
+    }
+}
