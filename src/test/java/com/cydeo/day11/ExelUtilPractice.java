0708e59b5a27a5cfbc7566879f89b676b9633d1d
hrnoksz <harun1973@yahoo.com>
2022-08-01 10:24:55 +0300

    Day11 ExelUtilPractice.java is done

--- src/test/java/com/cydeo/day11/ExelUtilPractice.java
+++ src/test/java/com/cydeo/day11/ExelUtilPractice.java
@@ -3,6 +3,9 @@ package com.cydeo.day11;
 import com.cydeo.utililities.ExcelUtil;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 public class ExelUtilPractice {
 
     @Test
@@ -13,5 +16,11 @@ public class ExelUtilPractice {
         //Argument 1: location of the file(path)
         //Argument 2: sheet that we want to open
         ExcelUtil vytrackFile = new ExcelUtil("src/test/resources/Vytracktestdata.xlsx", "QA3-short");
+
+        //method for returning list of map!!!!!!!!!!!!!!!!!!!!!!!!
+        List<Map<String, String>> dataList = vytrackFile.getDataList();
+        for (Map<String, String> rowMap : dataList) {
+            System.out.println("rowMap = " + rowMap);
+        }
     }
 }
