e13fa7b7a2665dcd574bfb01fa52a249e2980dea
hrnoksz <harun1973@yahoo.com>
2022-08-04 15:11:08 +0300

    Day12: MockApi.java is done

--- src/test/java/com/cydeo/day12/MockApi.java
+++ src/test/java/com/cydeo/day12/MockApi.java
@@ -1,4 +1,40 @@
 package com.cydeo.day12;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class MockApi {
+
+    //https://d2c37f22-1f84-48f6-86ad-08c73e12c9c0.mock.pstmn.io
+
+    @Test
+    public void test1(){
+
+        given().baseUri("https://d2c37f22-1f84-48f6-86ad-08c73e12c9c0.mock.pstmn.io")
+                .accept(ContentType.JSON)
+        .when()
+                .get("/customer")
+
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("firstName", is("John"));
+    }
+
+    @Test
+    public void test2(){
+
+        given().baseUri("https://d2c37f22-1f84-48f6-86ad-08c73e12c9c0.mock.pstmn.io")
+                .accept(ContentType.JSON)
+        .when()
+                .get("/employees")
+                .prettyPrint();
+    }
+
 }
